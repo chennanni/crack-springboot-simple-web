4bb4bb4953e5aafe57e65674cf55eaee3e99568a
Chennan <chennanni@users.noreply.github.com>
2023-09-26 17:47:08 +0800

    simple spring boot web app

---
+++ src/main/java/com/example/springbootsimpleweb/SpringbootSimpleWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootsimpleweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSimpleWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSimpleWebApplication.class, args);
+    }
+
+}
